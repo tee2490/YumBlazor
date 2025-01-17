e1f5e06266134f6e5c157338c2ca753543531c54
Teeradet <tee2490@hotmail.com>
2025-01-17 14:43:43 +0700

    Section 11 - Project Setup

--- YumBlazor/Data/ApplicationDbContext.cs
+++ YumBlazor/Data/ApplicationDbContext.cs
@@ -5,5 +5,13 @@ namespace YumBlazor.Data
 {
     public class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : IdentityDbContext<ApplicationUser>(options)
     {
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            base.OnConfiguring(optionsBuilder);
+            optionsBuilder.UseSqlServer("Server=Teeradet;Database=YumBlazor;Trusted_Connection=True;MultipleActiveResultSets=true;TrustServerCertificate=True");
+        }
     }
+
+    
 }
