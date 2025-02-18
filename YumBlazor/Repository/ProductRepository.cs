baeae009f9be3fd0545cf658cd06a5c210ae10f2
Teeradet <tee2490@hotmail.com>
2025-02-18 15:41:47 +0700

    Section 3 - Delete Product in Action

--- YumBlazor/Repository/ProductRepository.cs
+++ YumBlazor/Repository/ProductRepository.cs
@@ -3,10 +3,12 @@
     public class ProductRepository : IProductRepository
     {
         private readonly ApplicationDbContext _db;
+        private readonly IWebHostEnvironment _webHostEnvironment;
 
-        public ProductRepository(ApplicationDbContext db)
+        public ProductRepository(ApplicationDbContext db, IWebHostEnvironment webHostEnvironment)
         {
             _db = db;
+            _webHostEnvironment = webHostEnvironment;
         }
         public async Task<Product> CreateAsync(Product obj)
         {
@@ -18,6 +20,12 @@
         public async Task<bool> DeleteAsync(int id)
         {
             var obj = await _db.Product.FirstOrDefaultAsync(u => u.Id == id);
+            var imagePath = Path.Combine(_webHostEnvironment.WebRootPath, obj.ImageUrl.TrimStart('/'));
+            if (File.Exists(imagePath))
+            {
+                File.Delete(imagePath);
+            }
+
             if (obj != null)
             {
                 _db.Product.Remove(obj);
