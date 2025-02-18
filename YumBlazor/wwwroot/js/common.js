c2a4aaa6ed4186976b40bfcd72e476692a821b56
Teeradet <tee2490@hotmail.com>
2025-02-18 11:30:26 +0700

    Section 2 - Prepare to show and hide Modal

---
+++ YumBlazor/wwwroot/js/common.js
@@ -0,0 +1,6 @@
+﻿function ShowConfirmationModal() {
+    bootstrap.Modal.getOrCreateInstance(document.getElementById('exampleModal')).show();
+}
+function HideConfirmationModal() {
+    bootstrap.Modal.getOrCreateInstance(document.getElementById('exampleModal')).hide();
+}
