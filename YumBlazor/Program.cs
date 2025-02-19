1be0ab3ba9ee7d0a0da3f707032d54f264ccc2dd
Teeradet <tee2490@hotmail.com>
2025-02-19 08:36:43 +0700

    Section 4 - Register with Roles in Action

--- YumBlazor/Program.cs
+++ YumBlazor/Program.cs
@@ -39,7 +39,7 @@ builder.Services.AddIdentityCore<ApplicationUser>(options => {
     options.Password.RequireLowercase = false;
     options.Password.RequireUppercase = false;
     options.Password.RequireNonAlphanumeric = false;
-})
+}).AddRoles<IdentityRole>()
     .AddEntityFrameworkStores<ApplicationDbContext>()
     .AddSignInManager()
     .AddDefaultTokenProviders();
