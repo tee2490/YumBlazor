806f1918cff31fe1a8ebd0a88155ed3ed1c40b5a
Teeradet <tee2490@hotmail.com>
2025-02-21 19:11:29 +0700

    Section 11- Add more endpoints in Order Repository

--- YumBlazor/Repository/IRepository/IOrderRepository.cs
+++ YumBlazor/Repository/IRepository/IOrderRepository.cs
@@ -5,6 +5,7 @@
         public Task<OrderHeader> CreateAsync(OrderHeader orderHeader);
         public Task<OrderHeader> GetAsync(int id);
         public Task<IEnumerable<OrderHeader>> GetAllAsync(string? userId = null);
-        public Task<OrderHeader> UpdateStatusAsync(int orderId, string status);
+        public Task<OrderHeader> UpdateStatusAsync(int orderId, string status, string paymentIntentId);
+        public Task<OrderHeader> GetOrderBySessionIdAsync(int sessionId);
     }
 }
